d6528825c5f75be39ef4d69699bc175143ba2c0a
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-13 18:52:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ require 'pry'
 def oxford_comma(array)
   output=[]
   output.unshift(array.pop())
-  binding.pry
+  
   output.unshift(array.join(","))
-
+binding.pry
   output.join("and")
 output
 end
