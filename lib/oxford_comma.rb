d55b308587c9fc2713646ddecb8ca1c9512aa7ef
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-13 19:22:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@
 
 def oxford_comma(array)
 output=""
-  if array.size=1
+  if array.size==1
     output=array.join()
-  elsif array.size=2
+  elsif array.size==2
     output=array.join(" and ")
   else
     array.insert(-2, "and").join(", ").sub("and,", "and")
